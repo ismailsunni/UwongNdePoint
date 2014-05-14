67c98bf49b59e8cc60f74d15aa14e08ad3359c46
Listiarso Wastuargo <hallucinogenplus@gmail.com>
2014-05-13 19:36:29 -0700

    Android custom marker and zoom to position

--- UwongNdePoint.Android/MainActivity.cs
+++ UwongNdePoint.Android/MainActivity.cs
@@ -6,6 +6,8 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.Locations;
 
 namespace UwongNdePoint
 {
@@ -19,6 +21,32 @@ namespace UwongNdePoint
       base.OnCreate(bundle);
 
       SetContentView(Resource.Layout.Main);
+
+      MapFragment mapFragment = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.map);
+      GoogleMap map = mapFragment.Map;
+      // TODO: move this to a listener instead
+      map.MyLocationEnabled = true;
+
+      var location = map.MyLocation;
+      if (location == null)
+      {
+        LocationManager locationManager = (LocationManager) GetSystemService(Service.LocationService);
+        Criteria criteria = new Criteria();
+        String provider = locationManager.GetBestProvider(criteria, true);
+        location = locationManager.GetLastKnownLocation(provider);
+      }
+      if (location != null)
+      {
+        // TODO: get marker from Parse
+        var latlong = new LatLng(location.Latitude, location.Longitude);
+        map.AnimateCamera(CameraUpdateFactory.NewLatLngZoom(latlong, 16));
+
+        MarkerOptions markerOption = new MarkerOptions();
+        markerOption.SetPosition(latlong);
+        markerOption.SetTitle("Ganteng");
+        markerOption.InvokeIcon(BitmapDescriptorFactory.FromResource(Resource.Drawable.Icon));
+        map.AddMarker(markerOption);
+      }
     }
   }
 }
